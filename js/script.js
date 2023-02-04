9375e03e1f8d5812a3a0ec2426c0fc2824bcb9b3
Suganya-arun <100554621+Suganya-arun@users.noreply.github.com>
2023-02-04 22:07:38 +0530

    Added file via upload

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
-var io="suganya";
+// var io="suganya";
+var selectedValue=0;
+const button = document.getElementsByClassName("rating");
+// console.log(button);
+for (i = 0; i < button.length; i++) {
+    button[i].addEventListener("click", (e) => {
+        selectedValue = e.target.value;
+    });
+}
+
+const form = document.getElementById("ratings");
+    // console.log(form);
+    form.addEventListener("submit", ratingFunction);
+
+function ratingFunction(e) {
+    e.preventDefault();
+    //console.log(selectedValue);
+    document.querySelector("span").innerHTML=selectedValue;
+    const overlay=document.getElementsByClassName("container");
+    overlay[0].style.zIndex="0";
+
+}
+
